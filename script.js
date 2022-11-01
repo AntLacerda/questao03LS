30a7fcd0ad584396d8c42bb8d8eb1ae636707604
AntLacerda <lacerdaneto99@hotmail.com>
2022-11-01 12:59:25 -0300

    criando os arquivos

---
+++ script.js
@@ -0,0 +1,54 @@
+let val_celsius = document.querySelector("#celsius");
+let resp_fah = document.querySelector("#resp_fah");
+let resp_kel = document.querySelector("#resp_kel");
+let txt = document.querySelector("#txt_valor");
+
+const fahrenheit = (celsius) => {
+    if(isNaN(celsius)||celsius===0){
+        if(celsius===0){
+            resp_fah.innerHTML = "-";
+            txt.style.display = "inline";
+            txt.innerHTML = "Digite um valor!";
+        }
+        else{
+            resp_fah.innerHTML = "-";
+            txt.style.display = "inline";
+            txt.innerHTML = "Digite um valor válido!";
+        }
+    }
+    else{
+        let resp = (celsius * 1.8) + 32;
+        console.log(resp);
+        resp_fah.innerHTML = resp;
+        txt.style.display = "none";
+    }
+}
+
+const kelvin = (celsius) => {
+    if(isNaN(celsius)||celsius===0){
+        if(celsius===0){
+            resp_kel.innerHTML = "-";
+            txt.style.display = "inline";
+            txt.innerHTML = "Digite um valor!";
+        }
+        else{
+            resp_kel.innerHTML = "-";
+            txt.style.display = "inline";
+            txt.innerHTML = "Digite um valor válido!";
+        }
+    }
+    else{
+        let resp = celsius + 273.15;
+        console.log(resp);
+        resp_kel.innerHTML = resp;
+        txt.style.display = "none";
+    }
+}
+
+val_celsius.addEventListener('keyup', function(){
+    console.log(val_celsius.value);
+    fahrenheit(Number(val_celsius.value));
+    kelvin(Number(val_celsius.value));
+});
+
+
